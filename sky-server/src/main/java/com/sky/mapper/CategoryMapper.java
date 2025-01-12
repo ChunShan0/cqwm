bd87c9f4b2ffa1e258128bb1a01f8d3fcb3fab71
yzm <2694115925@qq.com>
2025-01-12 20:51:18 +0800

    公共字段自动填充功能

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
+import com.sky.enumeration.OperationType;
 import com.sky.result.PageResult;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -18,6 +20,7 @@ public interface CategoryMapper {
      */
     @Insert("insert into sky_take_out.category (type, name, sort, create_time, update_time, create_user, update_user) " +
             "values (#{type}, #{name}, #{sort}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    @AutoFill(value = OperationType.INSERT)
     void save(Category category);
 
 
@@ -32,6 +35,7 @@ public interface CategoryMapper {
      * 修改分类
      * @param category
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Category category);
 
     /**
